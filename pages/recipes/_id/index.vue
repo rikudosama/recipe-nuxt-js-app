3ae315f57cb58d1f415b6bd628446d29ea9e3649
lerikudosama <bjean633@gmail.com>
2018-09-15 03:15:49 +0000

    add new online photos link instead of local link

--- pages/recipes/_id/index.vue
+++ pages/recipes/_id/index.vue
@@ -24,21 +24,21 @@ export default {
                             id: "1",
                             title: "Savoureuse pizza",
                             previewDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-                            thumbnail: "/assets/img/pizza.jpg",
+                            thumbnail: "http://www.wellandpizza.com/wp-content/uploads/2017/11/Lucianos-Pizzeria-Welland-Pizza4.jpg",
                             description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore velit aut reiciendis laborum totam. Perspiciatis eligendi veniam voluptatibus provident fuga facilis enim sequi voluptas mollitia! Laboriosam voluptatem nihil quia placeat?"
                         },
                         {
                             id: "2",
                             title: "Un big belly burger",
                             previewDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-                            thumbnail: "/assets/img/humberger.jpg",
+                            thumbnail: "http://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/master/pass/the-ultimate-hamburger.jpg",
                             description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore velit aut reiciendis laborum totam. Perspiciatis eligendi veniam voluptatibus provident fuga facilis enim sequi voluptas mollitia! Laboriosam voluptatem nihil quia placeat?"
                         },
                         {
                             id: "3",
                             title: "Une salade 100% bio",
                             previewDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-                            thumbnail: "/assets/img/salade.jpg",
+                            thumbnail: "https://cdn2.tmbi.com/TOH/Images/Photos/37/1200x1200/Holiday-Lettuce-Salad_exps41195_CW132792B07_09_6bC_RMS.jpg",
                             description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore velit aut reiciendis laborum totam. Perspiciatis eligendi veniam voluptatibus provident fuga facilis enim sequi voluptas mollitia! Laboriosam voluptatem nihil quia placeat?"
                         }
                     ].find(el => el.id === context.params.id)
--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -4,6 +4,7 @@
         v-for="recipe in recipes"
         :key="recipe.id"
         :title="recipe.title"
+        :thumbnail="recipe.thumbnail"
         :previewDescription="recipe.previewDescription"
         :id="recipe.id"
         />
@@ -24,19 +25,19 @@ export default {
                             id: "1",
                             title: "Savoureuse pizza",
                             previewDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-                            thumbnail: "/assets/img/pizza.jpg"
+                            thumbnail: "http://www.wellandpizza.com/wp-content/uploads/2017/11/Lucianos-Pizzeria-Welland-Pizza4.jpg"
                         },
                         {
                             id: "2",
                             title: "Un big belly burger",
                             previewDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-                            thumbnail: "/assets/img/humberger.jpg"
+                            thumbnail: "http://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/master/pass/the-ultimate-hamburger.jpg"
                         },
                         {
                             id: "3",
                             title: "Une salade 100% bio",
                             previewDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-                            thumbnail: "/assets/img/salade.jpg"
+                            thumbnail: "https://cdn2.tmbi.com/TOH/Images/Photos/37/1200x1200/Holiday-Lettuce-Salad_exps41195_CW132792B07_09_6bC_RMS.jpg"
                         }
                     ]
                 })

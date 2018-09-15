a2da73817424a1d525184b4c82c35727c6819f75
lerikudosama <bjean633@gmail.com>
2018-09-15 03:24:00 +0000

    custom likn for github

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <a href="https://github.com/rikudosama/recipe-nuxt-js-app" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
